a973389a24804437b186bab839e43169ea7cf9e9
mengjuncheng <mengjuncheng@nash.work>
2019-12-01 18:59:35 +0800

    update

--- Formula/php71-intl.rb
+++ Formula/php71-intl.rb
@@ -7,7 +7,7 @@ class Php71Intl < AbstractPhp71Extension
   revision 25
 
   bottle do
-    sha256 "6b712a59d6b9dfb79112967f1fc8e2113cc3f16b9932d50795750fd1d2ba2630" => :high_sierra
+    sha256 "f80a795a09328a9441bae4a8a60fa0d6d43ec5adc98f5aa5f51d06f4522c07fe" => :high_sierra
     sha256 "f8056ad88e3da5272876ff0639c331883db62fe7d0b9389f68d56420b4445665" => :sierra
     sha256 "0bf6a1caecb8d7a92afbd90022b168ebebefe6c14636e208bb3481eba3501da0" => :el_capitan
   end
